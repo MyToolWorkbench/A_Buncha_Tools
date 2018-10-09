f267935778a7e2d2db649fd19968971e6deb7a48
ItsAMeNick <zonern@rpi.edu>
2018-10-09 10:31:40 -0400

    [2] HTML and CSS for login page. Added statics directory Added mTW-Full.jpg Added login.css Changed login.html - Added Image - Added 'Remember Me?' - Added 'Forgot Password' - Styled fields - Styled page

--- A_Buncha_Tools/settings.py
+++ A_Buncha_Tools/settings.py
@@ -121,4 +121,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
+STATICFILES_DIRS = (
+  os.path.join(SITE_ROOT, 'static/'),
+)
+
 LOGIN_REDIRECT_URL = '/'
