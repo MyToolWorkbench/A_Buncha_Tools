cf265d7e83a29c3dd7fd9a963146ac4da528932c
Anthony <ieatbeens@gmail.com>
2018-10-31 14:06:43 -0400

    [2] Merge branch 'master' of https://github.com/MyToolWorkbench/A_Buncha_Tools.git
    # Conflicts:
    #	.gitignore

--- myToolWorkbench/models.py
+++ myToolWorkbench/models.py
@@ -1,3 +1,41 @@
 from django.db import models
-
+from enum import Enum
 # Create your models here.
+
+""""
+class Country(Enum):
+    USA = "USA"
+    CHINA = "CHN"
+    TAIWAN = "TWN"
+    INDONESIA = "IDN"
+    MALAYSIA = "MAL"
+
+"""
+
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    phone_number = models.CharField(max_length=30)
+
+
+class User(Person):
+    username = models.CharField(max_length=30)
+
+
+class Tool(models.Model):
+    part_number = models.CharField(max_length=30)
+    description = models.TextField()
+    cost = models.DecimalField(max_digits=7, decimal_places=2)
+    special_retail = models.DecimalField(max_digits=7, decimal_places=2)
+    regular_retail = models.DecimalField(max_digits=7, decimal_places=2)
+    # catalogue_page
+    # tw_page
+    # UOM
+    upc_code = models.IntegerField(max_length=12)
+    weight = models.DecimalField(max_digits=7, decimal_places=2)
+    length = models.DecimalField(max_digits=7, decimal_places=2)
+    width = models.DecimalField(max_digits=7, decimal_places=2)
+    height = models.DecimalField(max_digits=7, decimal_places=2)
+    # origin_country
+
